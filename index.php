b2d390f00671d75460bad8afbec5f42fac979625
claudiogallego <claudio.gallego@gmail.com>
2021-01-14 15:04:55 -0300

    teste1234

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 
 
 <FONT FACE = "ARIAL" COLOR= "BLACK " SIZE = "10">DIADEMA EH CORINTHIANS !!! </FONT>
+
+
+<a href="https://api.whatsapp.com/send?phone=5519999888799&text=link%20para%20whatsapp">link para whatsapp</a>
